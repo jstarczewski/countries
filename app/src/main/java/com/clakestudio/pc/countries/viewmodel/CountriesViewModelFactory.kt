35cfbf3e83b5d2534385734229cb26387636d635
jstarczewski <jan.a.starczewski@gmail.com>
2019-05-02 18:50:58 +0200

    ViewModel factory implemented

--- app/src/main/java/com/clakestudio/pc/countries/viewmodel/CountriesViewModelFactory.kt
+++ app/src/main/java/com/clakestudio/pc/countries/viewmodel/CountriesViewModelFactory.kt
@@ -2,13 +2,23 @@ package com.clakestudio.pc.countries.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import javax.inject.Inject
+import javax.inject.Provider
 
 
-class CountriesViewModelFactory() : ViewModelProvider.Factory {
-
-
+class CountriesViewModelFactory @Inject constructor(
+        private val creators: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
+) : ViewModelProvider.Factory {
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val creator = creators[modelClass] ?: creators.entries.firstOrNull {
+            modelClass.isAssignableFrom(it.key)
+        }?.value ?: throw IllegalArgumentException("unknown model class $modelClass")
+        try {
+            @Suppress("UNCHECKED_CAST")
+            return creator.get() as T
+        } catch (e: Exception) {
+            throw RuntimeException(e)
+        }
     }
 
 
