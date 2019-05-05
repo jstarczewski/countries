91e47393c344c7649b52f8460444fcbde248cc11
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-05-05 15:26:46 +0200

    Widgets positions changed in DetailsFragment

--- app/src/main/java/com/clakestudio/pc/countries/di/AppModule.kt
+++ app/src/main/java/com/clakestudio/pc/countries/di/AppModule.kt
@@ -1,5 +1,6 @@
 package com.clakestudio.pc.countries.di
 
+import android.app.Application
 import com.clakestudio.pc.countries.data.source.CountryDataSource
 import com.clakestudio.pc.countries.data.CountryRepository
 import com.clakestudio.pc.countries.data.source.remote.CountriesRemoteDataSource
@@ -7,10 +8,14 @@ import com.clakestudio.pc.countries.data.source.remote.CountriesRestAdapter
 import com.clakestudio.pc.countries.data.source.remote.URLManager
 import dagger.Module
 import dagger.Provides
+import okhttp3.Cache
+import okhttp3.CacheControl
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
@@ -19,12 +24,31 @@ class AppModule {
 
     @Provides
     @Singleton
-    fun provideRetrofit(): Retrofit {
+    fun provideRetrofit(/*application: Application*/): Retrofit {
+/*
+        val cacheSize = 10 * 1024 * 1024 // 10 MB
+        val httpCacheDirectory = File(application.cacheDir, "http-cache")
+        val cache = Cache(httpCacheDirectory, cacheSize.toLong())
+
+        val networkCacheInterceptor = Interceptor { chain ->
+            val response = chain.proceed(chain.request())
+
+            var cacheControl = CacheControl.Builder()
+                .maxAge(1, TimeUnit.MINUTES)
+                .build()
+
+            response.newBuilder()
+                .header("Cache-Control", cacheControl.toString())
+                .build()
+        }*/
+
         val okHttpClient = OkHttpClient.Builder()
-            .connectTimeout(10, TimeUnit.MILLISECONDS)
-            .writeTimeout(1, TimeUnit.SECONDS)
-            .readTimeout(1, TimeUnit.SECONDS)
-            .retryOnConnectionFailure(false)
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(10, TimeUnit.SECONDS)
+     //       .addNetworkInterceptor(networkCacheInterceptor)
+       //     .cache(cache)
+            .retryOnConnectionFailure(true)
             .build()
         return Retrofit.Builder()
             .client(okHttpClient)
