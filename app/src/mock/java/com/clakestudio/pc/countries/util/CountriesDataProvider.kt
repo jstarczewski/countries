49d2aa4b89779f97aa3f6a95bd4700e2710f0818
Jan Starczewski <jan.a.starczewski@gmail.com>
2019-05-05 23:31:51 +0200

    Two test objects added

--- app/src/mock/java/com/clakestudio/pc/countries/util/CountriesDataProvider.kt
+++ app/src/mock/java/com/clakestudio/pc/countries/util/CountriesDataProvider.kt
@@ -79,4 +79,77 @@ object CountriesDataProvider {
             ),
             "COL"
         )
+
+    fun providePoland() =
+        Country(
+
+
+            name = "Poland",
+            topLevelDomain = listOf(".pl"),
+            alpha2Code = "PL",
+            alpha3Code = "POL",
+            callingCodes = listOf("48"),
+            capital = "Warsaw",
+            altSpellings = listOf(
+                "PL",
+                "Republic of Poland",
+                "Rzeczpospolita Polska"
+            ),
+            region = "Europe",
+            subregion = "Eastern Europe",
+            population = "38437239",
+            latlng = listOf("52.0", "20.0"),
+            demonym = "Polish",
+            area = "312679.0",
+            gini = "34.1",
+            timezones = listOf("UTC+01=00"),
+            borders = listOf(
+                "BLR",
+                "CZE",
+                "DEU",
+                "LTU",
+                "RUS",
+                "SVK",
+                "UKR"
+            ),
+            nativeName = "Polska",
+            numericCode = "616",
+            currencies = listOf(
+                Country.Currency(
+                    code = "PLN",
+                    name = "Polish złoty",
+                    symbol = "zł"
+                )
+            ),
+            languages = listOf(
+                Country.Language(
+                    iso639_1 = "pl",
+                    iso639_2 = "pol",
+                    name = "Polish",
+                    nativeName = "język polski",
+                    )
+            ),
+            translations = mapOf(
+                "de" to "Polen",
+                "es" to "Polonia",
+                "fr" to "Pologne",
+                "ja" to "ポーランド",
+                "it" to "Polonia",
+                "br" to "Polônia",
+                "pt" to "Polónia",
+                "nl" to "Polen",
+                "hr" to "Poljska",
+                "fa" to "هستان"
+            ),
+            flag = "https://restcountries.eu/data/pol.svg",
+            regionalBlocs = listOf(
+                Country.RegionalBlocs(
+                    acronym = " EU ",
+                    name = " European Union",
+                    otherAcronyms = listOf(),
+                    otherNames = listOf()
+                )
+            ),
+            cioc = "POL"
+        )
 }
